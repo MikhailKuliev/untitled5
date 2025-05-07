5ebba55ab950e7ccdf24424650847c90d8cfdde1
MikhailKuliev <mkh_3112@mail.ru>
2025-05-08 02:29:01 +0300

    фиксация изменений

--- src/User.java
+++ src/User.java
@@ -1,6 +1,18 @@
 public class User {
-    String name;
-    int age;
+    private String name;
+    private int age;
+    public User(String name, int age) {
+        this.name = name;
+        this.age = age;
+        System.out.println("User created");
+        switch (name) {
+            case "John":
+                System.out.println("John");
+                break;
+                case "Jane":
 
 
+                    System.out.println("Jane");
+        }
+    }
 }
