174dd395d413f41f2bff8277efb2001465cef3d5
MikhailKuliev <mkh_3112@mail.ru>
2025-05-12 01:31:03 +0300

    tusk

--- src/Main.java
+++ src/Main.java
@@ -2,20 +2,9 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-switch (args.length) {
-    case 1:
-        System.out.println(args[0]);
-        break;
-        case 2:
-            System.out.println(args[0] + " " + args[1]);
-            break;
-            case 3:
-                System.out.println(args[0] + " " + args[1] + " " + args[2]);
-                break;
-                default:
-                    break;
 
 
-}
+
+
     }
 }
