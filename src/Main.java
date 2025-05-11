51eae5999faebaa5519815aa13316cc5f5ca20fc
MikhailKuliev <mkh_3112@mail.ru>
2025-05-12 00:53:09 +0300

    создание библеотеки обьедениение коммитов

--- src/Main.java
+++ src/Main.java
@@ -3,34 +3,5 @@
 public class Main {
     public static void main(String[] args) {
 
-        Library library = new Library();
-// добавление книг
-        library.addBook(new Book("5000", "Лев толстой", 1901 ));
-        library.addBook(new Book("3124", "Пушкин", 1860 ));
-        // создание члена библеотеки
-        LibraryMember member = new LibraryMember("Alice", 1);
-        System.out.println(member.getMemberInfo());
-
-
-
-
-
-
-
-
-
-        BookAction action = new BookAction() {
-            @Override
-            public void performAction(Book book) {
-                System.out.println("выполнения действий: " + book.getInfo() );
-
-            }
-        };
-        for(Book book : library.getBooks()) {
-            System.out.println(book.getInfo());
-        }
-
-
     }
 }
-
